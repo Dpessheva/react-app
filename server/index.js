cd0b3cf51f04a1310034ce2d07409fac03a65db4
DPesheva <dpessheva@gmail.com>
2019-03-07 15:46:41 +0200

    update index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('/routes/auth');
 require('./database/database')();
 const port = 9999;
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -13,17 +16,14 @@ app.use((req, res, next) => {
   next();
 });
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 // General error handling
 app.use((error, req, res, next) => {
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({
-    message: message
-  });
+  res.status(status).json({ message: message });
   next();
-})
-
-app.listen(port, () => {
-  console.log(`REST API listening on port: ${port}`)
 });
+
+app.listen(port, () => { console.log(`REST API listening on port: ${port}`) });
