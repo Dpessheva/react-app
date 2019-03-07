fe293f6488c3831c5960dec155a1b92cc5c13592
DPesheva <dpessheva@gmail.com>
2019-03-07 15:30:29 +0200

    adding Furniture model

---
+++ server/models/Furniture.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const furnitureSchema = new Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Fruniture', furnitureSchema);
