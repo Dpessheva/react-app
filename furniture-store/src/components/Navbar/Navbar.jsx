ff2691924aa1c243e4bd9f2874299d8548335b7d
DPesheva <dpessheva@gmail.com>
2019-03-08 09:30:01 +0200

    adding navbar

---
+++ furniture-store/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout() {
+        this.props.handleLogout();
+    }
+    render() {
+        return (
+            <header>
+                <NavLink to="/" className="logo">The best Furniture store</NavLink>
+                <div className="header-right">
+                    <NavLink to="/">Home</NavLink>
+                    {
+                        this.props.username ?
+                            <span>
+                                <NavLink to="#">Welcome {this.props.username}!</NavLink>
+                                {
+                                    this.props.isAdmin ?
+                                        <span>
+                                            <NavLink to='/create'>Create</NavLink>
+                                        </span> :
+                                        null
+                                }
+                                <NavLink to='#' onClick={this.handleLogout}>Logout</NavLink>
+                            </span> :
+                            <span>
+                                <NavLink to='/register'>Register</NavLink>
+                                <NavLink to='/login'>Login</NavLink>
+                            </span>
+                    }
+
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Navbar;
