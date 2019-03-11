8086873c13128c2a820bb96e9f672d25225a70f9
DPesheva <dpessheva@gmail.com>
2019-03-11 17:38:04 +0200

    initial seed for candy

---
+++ candy-store/src/components/common/Candy/CandyCardList.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import CandyCard from './CandyCard'
+
+const CandyCardList = (props) => {
+  let allCandies = props.products
+  let candyCardList = []
+  for (let i = 0; i < allCandies.length; i += 3) {
+    let candyCards = allCandies.slice(i, Math.min(i + 3, allCandies.length))
+      .map(p => (
+        <CandyCard
+          key={p._id}
+          id={p._id}
+          name={p.name}
+          image={p.image}
+          description={p.description}
+          weight={p.weight} />))
+
+    let cardDeck = <div key={i} className='card-deck space-top'>{candyCards}</div>
+    candyCardList.push(cardDeck)
+  }
+
+  return (
+    <div className='row'>
+      {candyCardList}
+    </div>
+  )
+}
+
+export default CandyCardList
