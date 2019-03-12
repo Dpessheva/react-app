33ec7a9ad30b35964d07c632a996eb262d573d8b
DPesheva <dpessheva@gmail.com>
2019-03-12 10:10:17 +0200

    updates

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,34 +1,40 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const REQUIRED_VALIDATION_MESSAGE = '{PATH} is required'
+
 const productSchema = new Schema({
 
-  type: {
+  name: {
     type: String,
     required: true,
     unique: [true, 'Product already exists.']
   },
   description: {
     type: String,
-    required: true
+    required: REQUIRED_VALIDATION_MESSAGE
   },
   imageUrls: {
     type: String,
+    required: REQUIRED_VALIDATION_MESSAGE
   },
   quantity: {
-     type: Number,
-      required: true,
-       min: 0,
-        max: 1000
- },
-
-  price: {
     type: Number,
     required: true,
-    min: 1,
-     max: 500000
+    min: 0,
+    max: 1000
   },
- 
+  price: {
+    type: Number,
+    required: REQUIRED_VALIDATION_MESSAGE,
+    min: 1,
+    max: 500000
+  },
+  likes: [{
+    type: mongoose.Schema.Types.String
+  }],
+  reviews: [],
+
 });
 
 module.exports = mongoose.model('product', productSchema);
